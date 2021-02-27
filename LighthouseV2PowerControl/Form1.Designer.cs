8947f707fb7ccc53c524cd6f12b45e70eca09156
Mixail <mixail.korchikov@mail.ru>
2021-02-27 18:51:18 +0300

    first step

---
+++ LighthouseV2PowerControl/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace LighthouseV2PowerControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(93, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.FormattingEnabled = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 41);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(571, 303);
+            this.lbStatus.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lbStatus;
+    }
+}
+
---
+++ LighthouseV2PowerControl/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+
+namespace LighthouseV2PowerControl
+{
+    public partial class Form1 : Form
+    {
+        private Guid service = Guid.Parse("00001523-1212-efde-1523-785feabcd124");
+        private Guid characteristic = Guid.Parse("00001525-1212-efde-1523-785feabcd124");
+        private byte activateByte = 0x01;
+        private byte deactivateByte = 0x00;
+        private DeviceInformationCollection GatDevices;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lbStatus.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SendByte(activateByte);
+        }
+
+        private async void SendByte(byte byte4send)
+        {
+            GatDevices = await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(service));
+            BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(GatDevices[1].Id);
+            GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
+
+            if (result.Status == GattCommunicationStatus.Success)
+            {
+                IReadOnlyList<GattDeviceService> serviceList = result.Services;
+                for (int i = 0; i < serviceList.Count; ++i)
+                {
+                    if (serviceList[i].Uuid != service) continue;
+
+                    GattCharacteristicsResult gattRes = await serviceList[i].GetCharacteristicsAsync();
+                    if (gattRes.Status == GattCommunicationStatus.Success)
+                    {
+                        IReadOnlyList<GattCharacteristic> characteristics = gattRes.Characteristics;
+                        DataWriter writer = new DataWriter();
+                        writer.WriteByte(deactivateByte);
+                        var openStatus = await serviceList[i].OpenAsync(GattSharingMode.SharedReadAndWrite);
+                        lbStatus.Items.Add(openStatus);
+                        for (int j = 0; j < characteristics.Count; ++j)
+                        {
+                            if(characteristics[j].Uuid != characteristic) continue;
+                            lbStatus.Items.Add(characteristics[j].CharacteristicProperties);
+                            var resWrite = await characteristics[j].WriteValueWithResultAsync(writer.DetachBuffer());
+                            lbStatus.Items.Add(resWrite.Status);
+                            //GattCommunicationStatus resWrite = await characteristics[j].WriteValueAsync(writer.DetachBuffer());
+                            //if (resWrite == GattCommunicationStatus.Success)
+                            //{
+                            //    lbStatus.Items.Add(resWrite);
+                            //}
+                            //else
+                            //{
+                            //    lbStatus.Items.Add(resWrite);
+                            //}
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
