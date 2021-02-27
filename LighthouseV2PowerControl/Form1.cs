0078eb52f18f4bbc4615cd89564bc9ce59ce3d79
Mixail <mixail.korchikov@mail.ru>
2021-02-27 20:15:47 +0300

    rdy 2 use

--- LighthouseV2PowerControl/Form1.cs
+++ LighthouseV2PowerControl/Form1.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
-using System.Xml;
 using Windows.Devices.Bluetooth;
-using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Devices.Enumeration;
 using Windows.Storage.Streams;
@@ -23,59 +15,89 @@ namespace LighthouseV2PowerControl
         private Guid characteristic = Guid.Parse("00001525-1212-efde-1523-785feabcd124");
         private byte activateByte = 0x01;
         private byte deactivateByte = 0x00;
-        private DeviceInformationCollection GatDevices;
+        
+        private Regex regex = new Regex("^LHB-.{8}");
+
+        private static List<GattCharacteristic> listGattCharacteristics = new List<GattCharacteristic>();
 
         public Form1()
         {
             InitializeComponent();
             lbStatus.Text = "";
+            btnStart.Click += (obj,e) => SendOnLighthouse(activateByte);
+            btnStop.Click += (obj,e) => SendOnLighthouse(deactivateByte);
+            btnStop.Enabled = btnStart.Enabled = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            SendByte(activateByte);
+            GetGattCharacteristics();
         }
 
-        private async void SendByte(byte byte4send)
+        private async void GetGattCharacteristics()
         {
-            GatDevices = await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(service));
-            BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(GatDevices[1].Id);
-            GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
-
-            if (result.Status == GattCommunicationStatus.Success)
+            DeviceInformationCollection GatDevices = await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(service));
+            for (int id = 0; id < GatDevices.Count; id++)
             {
-                IReadOnlyList<GattDeviceService> serviceList = result.Services;
-                for (int i = 0; i < serviceList.Count; ++i)
-                {
-                    if (serviceList[i].Uuid != service) continue;
+                if(!regex.IsMatch(GatDevices[id].Name)) continue;
 
-                    GattCharacteristicsResult gattRes = await serviceList[i].GetCharacteristicsAsync();
-                    if (gattRes.Status == GattCommunicationStatus.Success)
+                BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(GatDevices[id].Id);
+                GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
+
+                if (result.Status == GattCommunicationStatus.Success)
+                {
+                    IReadOnlyList<GattDeviceService> serviceList = result.Services;
+                    for (int i = 0; i < serviceList.Count; ++i)
                     {
-                        IReadOnlyList<GattCharacteristic> characteristics = gattRes.Characteristics;
-                        DataWriter writer = new DataWriter();
-                        writer.WriteByte(deactivateByte);
-                        var openStatus = await serviceList[i].OpenAsync(GattSharingMode.SharedReadAndWrite);
-                        lbStatus.Items.Add(openStatus);
-                        for (int j = 0; j < characteristics.Count; ++j)
+                        if (serviceList[i].Uuid != service) continue;
+                        GattCharacteristicsResult gattRes = await serviceList[i].GetCharacteristicsForUuidAsync(characteristic);
+                        if (gattRes.Status == GattCommunicationStatus.Success)
                         {
-                            if(characteristics[j].Uuid != characteristic) continue;
-                            lbStatus.Items.Add(characteristics[j].CharacteristicProperties);
-                            var resWrite = await characteristics[j].WriteValueWithResultAsync(writer.DetachBuffer());
-                            lbStatus.Items.Add(resWrite.Status);
-                            //GattCommunicationStatus resWrite = await characteristics[j].WriteValueAsync(writer.DetachBuffer());
-                            //if (resWrite == GattCommunicationStatus.Success)
-                            //{
-                            //    lbStatus.Items.Add(resWrite);
-                            //}
-                            //else
-                            //{
-                            //    lbStatus.Items.Add(resWrite);
-                            //}
+                            var openStatus = await serviceList[i].OpenAsync(GattSharingMode.SharedReadAndWrite);
+                            IReadOnlyList<GattCharacteristic> characteristics = gattRes.Characteristics;
+                            for (int j = 0; j < characteristics.Count; ++j)
+                            {
+                                if (characteristics[j].Uuid != characteristic) continue;
+                                listGattCharacteristics.Add(characteristics[j]);
+                            }
                         }
                     }
                 }
             }
+
+            Log($"lighthouses found: {listGattCharacteristics.Count}");
+            if (listGattCharacteristics.Count > 0)
+            {
+                btnStop.Enabled = btnStart.Enabled = true;
+            }
+        }
+
+        private async void SendOnLighthouse(byte byte4send)
+        {
+            for (int i = 0; i < listGattCharacteristics.Count; ++i)
+            {
+                DataWriter writer = new DataWriter();
+                writer.WriteByte(byte4send);
+                GattCommunicationStatus resWrite = await listGattCharacteristics[i].WriteValueAsync(writer.DetachBuffer());
+                if (resWrite == GattCommunicationStatus.Success)
+                {
+                    Log($"Success");
+                }
+                else
+                {
+                    LogError($"lighthouse: {resWrite}");
+                }
+            }
+        }
+
+        private void Log(object msg)
+        {
+            lbStatus.Items.Add(msg);
+        }
+
+        private void LogError(object msg)
+        {
+            lbStatus.Items.Add(msg);
         }
     }
 }
